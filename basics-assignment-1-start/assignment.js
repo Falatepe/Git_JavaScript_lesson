a4ae329f9aeaa97f2f62f6e6f81c9f46a1cbbb1b
Falatepe <fatihalatepe@gmail.com>
2020-04-09 23:03:51 +0200

    continue

--- basics-assignment-1-start/assignment.js
+++ basics-assignment-1-start/assignment.js
@@ -1,7 +1,13 @@
 let firstValue = 1;
-let resultValue = ;
+let resultValue;
+
 resultValue = 18;
+
 resultValue = 18-firstValue;
+
 resultValue = resultValue*2;
+
 resultValue = resultValue/3;
-alert(resultValue)
+
+alert(resultValue);
+alert(firstValue);
